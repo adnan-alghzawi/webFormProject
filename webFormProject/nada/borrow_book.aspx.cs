12cb5047b03ba9ee8efd0b7e0cc8f802d8e4d291
Nada Kdesat <nadaqdesat@gmail.com>
2025-02-02 16:50:32 +0300

    nada

--- webFormProject/nada/borrow_book.aspx.cs
+++ webFormProject/nada/borrow_book.aspx.cs
@@ -102,7 +102,6 @@ namespace webFormProject.nada
                             "        window.location = 'show_books.aspx'; " +  
                             "    } " +
                             "});", true);
-                            
                         }
                         else
                         {
