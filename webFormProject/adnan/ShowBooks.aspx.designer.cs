25c968d23deb6e84a3e0fb7b8e9d48cdb6f04fb1
adnan alghzawi <adnanalghzawi7@gmail.com>
2025-02-02 09:51:46 +0300

    7

--- webFormProject/adnan/ShowBooks.aspx.designer.cs
+++ webFormProject/adnan/ShowBooks.aspx.designer.cs
@@ -51,6 +51,15 @@ namespace webFormProject.adnan
         protected global::System.Web.UI.WebControls.Button btnEdit;
 
         /// <summary>
+        /// btnAddBook control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddBook;
+
+        /// <summary>
         /// rptBooks control.
         /// </summary>
         /// <remarks>
