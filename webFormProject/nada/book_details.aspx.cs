467c30e6ad127de94a7e1166ae58fa104b0e66bc
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-02-03 23:26:37 +0300

    commit final

--- webFormProject/nada/book_details.aspx.cs
+++ webFormProject/nada/book_details.aspx.cs
@@ -103,27 +103,27 @@ namespace webFormProject.nada
 
         protected void homeTab_Click(object sender, EventArgs e)
         {
-            Response.Redirect("./jana/index.aspx");
+            Response.Redirect("~/jana/index.aspx");
         }
 
         protected void aboutTab_Click(object sender, EventArgs e)
         {
-            Response.Redirect("../sally/aboutUs.aspx");
+            Response.Redirect("~/sally/about.aspx");
         }
 
         protected void contactTab_Click(object sender, EventArgs e)
         {
-            Response.Redirect("../suleiman/contactUs.aspx");
+            Response.Redirect("~/suleiman/contactUs.aspx");
         }
 
         protected void books_Click(object sender, EventArgs e)
         {
-            Response.Redirect("../nada/show_books.aspx");
+            Response.Redirect("~/nada/show_books.aspx");
         }
 
         protected void rooms_Click(object sender, EventArgs e)
         {
-            Response.Redirect("../jana/ViewRooms.aspx");
+            Response.Redirect("~/jana/ViewRooms.aspx");
         }
 
         protected void login_Click(object sender, EventArgs e)
--- webFormProject/nada/show_books.aspx.designer.cs
+++ webFormProject/nada/show_books.aspx.designer.cs
@@ -42,15 +42,6 @@ namespace webFormProject.nada
         protected global::System.Web.UI.WebControls.LinkButton aboutTab;
 
         /// <summary>
-        /// contactTab control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton contactTab;
-
-        /// <summary>
         /// books control.
         /// </summary>
         /// <remarks>
@@ -67,6 +58,15 @@ namespace webFormProject.nada
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton rooms;
+
+        /// <summary>
+        /// contactTab control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton contactTab;
 
         /// <summary>
         /// buttons control.
